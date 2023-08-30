a16d3b07a515a4bd6b7ef785ab0b014222050c80
Connor <connor.cs@protonmail.com>
2023-08-29 20:39:00 -0400

    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import {
   Artist,
   Track,
@@ -15,16 +15,19 @@ import Home from "./pages/Home";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [results, setResults] = useState<Artist[]>([]);
-  // const [searchType, setSearchType] = useState<SearchType>()
-  // const [artistResults, setArtistResults] = useState<Artist[]>();
-  // const [trackResults, setTrackResults] = useState<Track[]>()
-  
+  const [searchType, setSearchType] = useState<SearchType>()
+  const [artistResults, setArtistResults] = useState<Artist[]>();
+  const [trackResults, setTrackResults] = useState<Track[]>()
+
   // type SearchType = (<Track> | <Artist> | <Album>)
+
 
   const api = SpotifyApi.withClientCredentials(
     "1553a231a3b74e48bb3dc6efdce3cb72",
     "37da88f137294d5a9f6a7ea57f0d4be9"
   );
+
+  console.log(results);
 
   async function submit(e: React.BaseSyntheticEvent) {
     e.preventDefault();
@@ -33,18 +36,18 @@ function App() {
     setResults(items.artists.items);
     // settrackRes()
     //
+    console.log(results)
   }
 
   return (
     <main>
       <Nav submit={submit} handleChange={setSearchText} />
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home results={results}/>} />
-        <Route path="/artist/:id" element={<ArtistPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home results={results} />} />
+          <Route path="/artist/:id" element={<ArtistPage />} />
+        </Routes>
       </BrowserRouter>
-      
     </main>
   );
 }
--- src/pages/ArtistPage.tsx
+++ src/pages/ArtistPage.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card'
 
 const ArtistPage = () => {
   return (
     <div>
       <div className="albums-container dp-flex flex-row justify-content-center">
-        <div className="album border border-2 border-white p-4 text-light">album 1</div>
+        {/* <div className="album border border-2 border-white p-4 text-light">album 1</div>
         <div className="album border border-2 border-white p-4 text-light">album 2</div>
-        <div className="album border border-2 border-white p-4 text-light">album 3</div>
+        <div className="album border border-2 border-white p-4 text-light">album 3</div> */}
+        <Card key={1} className="card">
+          <Card.Img variant="top" src={'/default'} />
+          <Card.Body>
+            <Card.Title>Album 1</Card.Title>
+          </Card.Body>
+        </Card>
+        <Card key={2} className="card">
+          <Card.Img variant="top" src={'/default'} />
+          <Card.Body>
+            <Card.Title>Album 2</Card.Title>
+          </Card.Body>
+        </Card>
+        <Card key={3} className="card">
+          <Card.Img variant="top" src={'/default'} />
+          <Card.Body>
+            <Card.Title>Album 3</Card.Title>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   )
