9acb0725ae2d65f74547ad64f2a760e8f0f29548
Connor <connor.cs@protonmail.com>
2023-10-14 15:36:48 -0400

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,23 @@
+
+import { AiFillPlayCircle } from "react-icons/ai";
+import { IoPlaySkipForward, IoPlaySkipBack, IoPauseCircle } from 'react-icons/io5'
+
+
 const Footer = () => {
-  return(
-    <div className="footer ">
-      <div className="h4 text-light">This is the footer for playing songs</div>
+  return (
+    <div className="footer d-flex fixed-bottom justify-content-between mb-4">
+      {/* <div className="h4 text-light">This is the footer</div> */}
+      <div className="footer-left">
+        <p>song info</p>
+      </div>
+      <div className="footer-center">
+        <IoPlaySkipBack className="footer-icon m-2" size={50} />
+        <AiFillPlayCircle className="footer-icon m-2" size={50} />
+        <IoPlaySkipForward className="footer-icon m-2" size={50} />
+      </div>
+      <div className="footer-right">
+        <p>volume</p>
+      </div>
     </div>
   )
 }
