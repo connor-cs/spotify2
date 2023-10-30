7b7805f73611df56dd82ae38e4f850b184219721
Connor <connor.cs@protonmail.com>
2023-10-29 21:46:53 -0400

    more

--- src/components/SongRow.tsx
+++ src/components/SongRow.tsx
@@ -1,13 +1,17 @@
-const SongRow = ({track}) => {
+const SongRow = ({ track }) => {
   return (
     <div className="songRow d-flex">
-      <img className="songRow-album" src={track.album.images[2].url}/>
+      <img className="songRow-album" src={track.album.images[2].url} />
       <div className="songRow-info text-light">
         <h1 className="fs-4">{track.name}</h1>
-        <p>{track.artists[0].name} - {track.album.name}</p>
+        <div className="songRow-artist-album">
+          <p>{track.artists[0].name} </p>
+          <p> {track.album.name}</p>
+        </div>
+        {/* <p>{track.artists[0].name}{" "} {track.album.name}</p> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SongRow
+export default SongRow;
