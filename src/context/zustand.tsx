3e17d87bcc7a619c61e40b8f14d2b316b5c9b9f5
Connor <connor.cs@protonmail.com>
2023-12-25 00:00:07 -0500

    d

--- src/context/zustand.tsx
+++ src/context/zustand.tsx
@@ -1,5 +1,28 @@
 import { create } from "zustand";
-spotify-sdk:ClientCredentialsStrategy:token
-const context = create()
 
+//use this:
+// spotify-sdk:ClientCredentialsStrategy:token
+// or this?
+// access_token
+//
+//
+const useAuthStore = create((set) => ({
+  isAuthenticated: !!localStorage.getItem("access_token"),
+  accessToken: localStorage.getItem("access_token"),
+  currentTrack: "",
+  login: (token) => {
+    localStorage.setItem("access_token", token);
+    set({ isAuthenticated: true, accessToken: token });
+  },
+  logout: () => {
+    localStorage.removeItem("access_token");
+    set({ isAuthenticated: false, accessToken: null });
+  },
+  setCurrentTrack: (newTrackUri: string) => {
+    set((currentTrack) => ({
+      currentTrack: newTrackUri,
+    }));
+  },
+}));
 
+export default useAuthStore;
