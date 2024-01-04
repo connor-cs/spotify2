7c72d2f84f859417cea9d4aea41f1ca9a279293f
Connor <connor.cs@protonmail.com>
2024-01-03 20:46:09 -0500

    add song info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,10 +81,6 @@ const Footer: React.FC<SpotifyPlayerProps> = ({ uris }) => {
   //   }
   // };
 
-  const getTrackInfo = async (trackId) => {
-    const data = await fetch(`https://api.spotify.com/v1/tracks/${trackId}`)
-  }
-
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://sdk.scdn.co/spotify-player.js";
@@ -151,10 +147,6 @@ const Footer: React.FC<SpotifyPlayerProps> = ({ uris }) => {
     };
   }, [uris]);
 
-  //get track info to display at footer
-  useEffect(()=> {
-
-  },[])
 
   return (
     <div className="footer d-flex  justify-content-between mb-4">
